3e42686e9050fc1e27a11c19706f7a69ef6a368b
tomerKimberg <tomerkimberg@gmail.com>
2025-01-24 20:13:23 +0200

    added optimizer code from chatgpt need to test (should be added as deligate to the Layer class and used in method 'update_params'

---
+++ Optimizer.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+class Optimizer:
+    def __init__(self, method='SGD', lr=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.method = method
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = None  # Momentum for Adam/RMSprop
+        self.v = None  # RMSprop or Adam second moment
+
+    def update_params(self, weights, grad_weights, biases=None, grad_biases=None, use_bias=True):
+        if self.method == 'SGD':
+            # Standard SGD
+            weights -= self.lr * grad_weights
+            if use_bias:
+                biases -= self.lr * grad_biases
+
+        elif self.method == 'Momentum':
+            # SGD with Momentum
+            if self.m is None:
+                self.m = np.zeros_like(weights)
+            self.m = self.beta1 * self.m + self.lr * grad_weights
+            weights -= self.m
+            if use_bias:
+                if self.m is None:
+                    self.m = np.zeros_like(biases)
+                self.m = self.beta1 * self.m + self.lr * grad_biases
+                biases -= self.m
+
+        elif self.method == 'RMSprop':
+            # RMSprop
+            if self.v is None:
+                self.v = np.zeros_like(weights)
+            self.v = self.beta2 * self.v + (1 - self.beta2) * (grad_weights ** 2)
+            weights -= self.lr * grad_weights / (np.sqrt(self.v) + self.epsilon)
+            if use_bias:
+                if self.v is None:
+                    self.v = np.zeros_like(biases)
+                self.v = self.beta2 * self.v + (1 - self.beta2) * (grad_biases ** 2)
+                biases -= self.lr * grad_biases / (np.sqrt(self.v) + self.epsilon)
+
+        elif self.method == 'Adam':
+            # Adam Optimizer
+            if self.m is None:
+                self.m = np.zeros_like(weights)
+            if self.v is None:
+                self.v = np.zeros_like(weights)
+
+            self.m = self.beta1 * self.m + (1 - self.beta1) * grad_weights
+            self.v = self.beta2 * self.v + (1 - self.beta2) * (grad_weights ** 2)
+            m_hat = self.m / (1 - self.beta1)  # Bias correction
+            v_hat = self.v / (1 - self.beta2)  # Bias correction
+            weights -= self.lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
+
+            if use_bias:
+                if self.m is None:
+                    self.m = np.zeros_like(biases)
+                if self.v is None:
+                    self.v = np.zeros_like(biases)
+                self.m = self.beta1 * self.m + (1 - self.beta1) * grad_biases
+                self.v = self.beta2 * self.v + (1 - self.beta2) * (grad_biases ** 2)
+                m_hat = self.m / (1 - self.beta1)
+                v_hat = self.v / (1 - self.beta2)
+                biases -= self.lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
+
+        return weights, biases
